30b53dc7b688b2ed2afabad9b1087f9cbff6be9b
salayhin <salayhin@gmail.com>
2013-08-21 20:08:17 +0600

    Few modification done

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ end
 
 gem 'jquery-rails'
 
+gem 'better_errors'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,9 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+    @comment = Comment.new
+
+    @comment.article_id = @article.id
   end
 
   def create
