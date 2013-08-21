adcad962c17a2d5be2603b50f954d55127d66b6f
salayhin <salayhin@gmail.com>
2013-08-21 15:18:06 +0600

    article edit, delete done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,39 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
 
+  def create
+    @article = Article.new(params[:article])
+    @article.save
+
+    flash.notice = "Article '#{@article.title}' Created"
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 
 end
